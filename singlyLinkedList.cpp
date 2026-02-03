06ec1b3cf4a30ad3d7c92359a9a1db774d9c2ae3
jimmy <jimmysingh030704@gmail.com>
2026-02-03 23:32:01 +0530

    i have written code in cpp which insertsa a node at head and print the data of the nodes

--- singlyLinkedList.cpp
+++ singlyLinkedList.cpp
@@ -1,1 +1,48 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class Node{
+    public:
+    int data ;
+    Node* next;
+    Node(int d){
+        this->data = d;
+        this->next = NULL;
+
+    }
+   
+};
+
+void insertAtHead(Node* &head , int d){
+    Node* temp = new Node(d);
+    temp->next = head;
+    head = temp;
+
+}
+
+void print(Node* &head ){
+    Node* temp = head;
+    while(temp!=0){
+        cout<<temp->data<<endl;
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+
+int main(){
+    Node* node1 = new Node(10);
+    Node* head = node1;
+    print(head);
+    insertAtHead(head , 20);
+    print(head);
+    insertAtHead(head , 80);
+    print(head);
+    insertAtHead(head , 100);
+    print(head);
+    insertAtHead(head , 120);
+    print(head);
+    
+}
 
